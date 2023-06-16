0db7ef1679b86f5ec9e72f480e2bd2318f198fc3
Crash2022 <crash555@mail.ru>
2023-06-16 22:11:18 +0500

    add: post form double bind

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,55 @@
 <template>
-   <div class="app">
-       <form>
-           <h3 class="addPost_title">Создать пост</h3>
-           <input class="input" type="text" placeholder="Название">
-           <input class="input" type="text" placeholder="Описание">
-           <button class="add_button">Создать</button>
-       </form>
-       <div class="post" v-for="post of posts">
-           <div><strong>Название:</strong> {{post.title}}</div>
-           <div><strong>Описание:</strong> {{post.body}}</div>
-       </div>
-   </div>
+    <div class="app">
+        <form>
+            <h3 class="addPost_title">Создать пост</h3>
+            <input v-bind:value="title"
+                   @input="inputTitle"
+                   class="input"
+                   type="text"
+                   placeholder="Название"
+            >
+            <input v-bind:value="body"
+                   @input="inputBody"
+                   class="input"
+                   type="text"
+                   placeholder="Описание"
+            >
+            <button class="add_button" @click="createPost">
+                Создать
+            </button>
+        </form>
+        <div class="post" v-for="post of posts">
+            <div><strong>Название:</strong> {{ post.title }}</div>
+            <div><strong>Описание:</strong> {{ post.body }}</div>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                posts: [
-                    { id: 1, title: 'Javascript', body: 'Описание 1'},
-                    { id: 2, title: 'React', body: 'Описание 2'},
-                    { id: 3, title: 'Angular', body: 'Описание 3'},
-                ]
-            }
-        },
-        methods: {
+export default {
+    data() {
+        return {
+            posts: [
+                {id: 1, title: 'Javascript', body: 'Описание 1'},
+                {id: 2, title: 'React', body: 'Описание 2'},
+                {id: 3, title: 'Angular', body: 'Описание 3'},
+            ],
+            title: '',
+            body: '',
+        };
+    },
+    methods: {
+        createPost() {
 
+        },
+        inputTitle(e) {
+            this.title = e.target.value
+        },
+        inputBody(e) {
+            this.body = e.target.value
         }
     }
+};
 </script>
 
 <style>
@@ -40,18 +62,22 @@
 .app {
     padding: 20px;
 }
+
 .addPost_title {
     margin-bottom: 5px;
 }
+
 .post {
     border: 2px solid green;
     padding: 10px;
     margin-top: 10px;
 }
+
 .input {
     padding: 5px;
     margin-right: 5px;
 }
+
 .add_button {
     padding: 5px;
 }
