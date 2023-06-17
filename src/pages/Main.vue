631c335439d5232511802a74aaad6225b5304a56
Crash2022 <crash555@mail.ru>
2023-06-17 21:27:16 +0500

    add: new pages styles

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,7 +1,15 @@
 <template>
-    <h1>
-        Добро пожаловать!
-    </h1>
+    <div class="main_page">
+        <h1>
+            Добро пожаловать!
+        </h1>
+        <div>
+            <router-link to="/posts">Посты</router-link>
+        </div>
+        <div>
+            <router-link to="/about">Обо мне</router-link>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,8 +19,15 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-    text-align: center;
+.main_page {
     margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    font-size: 20px;
+}
+h1 {
+    margin-bottom: 20px;
 }
 </style>
