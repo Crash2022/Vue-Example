3dc64696f5c09ff9c138f6e8130a28fcfa075406
Crash2022 <crash555@mail.ru>
2023-06-17 11:28:03 +0500

    add: delete post v2

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <h3>Список пользователей</h3>
-        <post-item :post="post"
-                   v-for="post in posts"
+        <post-item v-for="post in posts"
+                   :post="post"
                    :key="post.id"
                    @delete="$emit('delete', post)"
         />
