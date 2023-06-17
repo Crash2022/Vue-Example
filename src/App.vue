321991721b253cfc5824a39903e26722127a711a
Crash2022 <crash555@mail.ru>
2023-06-17 21:46:30 +0500

    add: NavBar links v2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
+    <navbar/>
     <div class="app">
-        <navbar/>
         <router-view></router-view>
     </div>
 </template>
--- src/shared/ui/NavBar.vue
+++ src/shared/ui/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="navbar_wrapper">
-        <div>
-            <router-link to="/">Vue 3</router-link>
+        <div @click="$router.push('/')" class="main_link">
+            Vue 3
         </div>
         <nav class="navbar_links">
-            <div>
-                <router-link to="/posts">Посты</router-link>
+            <div class="post_button">
+                <custom-button @click="$router.push('/posts')">Посты</custom-button>
             </div>
             <div>
                 <router-link to="/about">Обо мне</router-link>
@@ -15,8 +15,11 @@
 </template>
 
 <script>
+import CustomButton from '@/shared/ui/CustomButton.vue';
+
 export default {
-    name: 'navbar'
+    name: 'navbar',
+    components: {CustomButton}
 };
 </script>
 
@@ -30,8 +33,21 @@ export default {
     box-shadow: 2px 2px 4px gray;
     padding: 0 20px;
 }
+.main_link:hover {
+    cursor: pointer;
+    color: white;
+}
 .navbar_links {
     display: flex;
+    align-items: center;
     gap: 20px;
+}
+.post_button button {
+    background-color: aliceblue;
+    color: black;
+}
+.post_button button:hover {
+    background-color: cornflowerblue;
+    color: white;
 }
 </style>
