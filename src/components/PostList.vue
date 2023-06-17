6967d720373559bf91d8805b61a8616836be4c90
Crash2022 <crash555@mail.ru>
2023-06-17 11:37:53 +0500

    add: if-else for posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,13 @@
 <template>
-    <div>
-        <h3>Список пользователей</h3>
+    <div v-if="posts.length > 0">
+        <h3 class="post_title">Список постов</h3>
         <post-item v-for="post in posts"
                    :post="post"
                    :key="post.id"
                    @delete="$emit('delete', post)"
         />
     </div>
+    <h3 v-else class="no_posts">Постов нет!</h3>
 </template>
 
 <script>
@@ -24,5 +25,13 @@ export default {
 </script>
 
 <style scoped>
-
+.post_title {
+    text-align: center;
+    margin-top: 20px;
+}
+.no_posts {
+    color: red;
+    text-align: center;
+    margin-top: 20px;
+}
 </style>
