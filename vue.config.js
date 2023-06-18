874984ed86dff046240967592547ec74fdcba5e2
Crash2022 <crash555@mail.ru>
2023-06-18 18:35:13 +0500

    add: sass webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  // resolve: {
-  //   extensions: ['.js', '.vue', '.json', '.scss'],
-  //   alias: {
-  //     'vue$': 'vue/dist/vue.esm.js',
-  //     '@': resolve('src'),
-  //     styles: resolve('src/shared/styles/')
-  //   }
-  // }
-})
+    transpileDependencies: true,
+});
