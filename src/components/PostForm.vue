16a4d1727b7c256450754bf6dc9d7bb28881aa55
Crash2022 <crash555@mail.ru>
2023-06-19 15:48:21 +0500

    add: AddPost with validation

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -3,29 +3,29 @@
         <h3 class="addPost_title">Создать пост</h3>
         <div class="add_form">
             <div class="form_input">
-                <custom-input v-model="post.title.value"
-                              :ref="post.title.ref"
+                <custom-input v-model="title.value"
+                              :ref="title.ref"
                               type="text"
                               placeholder="Название"
                               v-focus
                               name="title"
                 />
                 <!--<p v-if="title.error">{{ title.error.message }}</p>-->
-                <p v-if="post.title.error"
+                <p v-if="title.error"
                    class="input_error"
                 >
                     {{ 'Поле обязательно для заполнения' }}
                 </p>
             </div>
             <div class="form_input">
-                <custom-input v-model="post.body.value"
-                              :ref="post.body.ref"
+                <custom-input v-model="body.value"
+                              :ref="body.ref"
                               type="text"
                               placeholder="Описание"
                               name="body"
                 />
                 <!--<p v-if="body.error">{{ body.error.message }}</p>-->
-                <p v-if="post.body.error"
+                <p v-if="body.error"
                    class="input_error"
                 >
                     {{ 'Поле обязательно для заполнения' }}
@@ -46,38 +46,23 @@ import {useForm} from 'vue-hooks-form';
 
 export default {
     data() {
-        // return {
-        //     post: {
-        //         title: '',
-        //         body: ''
-        //     },
-        // }
+        return {
+
+        }
     },
     methods: {
-        // createPost() {
-        //     this.post.id = Date.now()
-        //
-        //     // подписка на событие (для передачи из дочерней в родительскую)
-        //     // название 'create' используется в родительской компонентне (@create)
-        //     // вторым параметром принимает post
-        //     this.$emit('create', this.post)
-        //
-        //     this.post = {
-        //         title: '',
-        //         body: '',
-        //     }
-        // },
+
     },
 
-    setup() {
+    setup(props, {emit}) {
         const {useField, handleSubmit} = useForm({
             defaultValues: {
-                // post: {
-                //     title: '',
-                //     body: ''
-                // },
+                title: '',
+                body: ''
             },
         })
+
+        const id = Date.now()
         const title = useField('title', {
             rule: {
                 required: true,
@@ -90,57 +75,50 @@ export default {
                 required: true,
             },
         })
-        const onSubmit = (data) => {
-            console.log(data)
 
-            this.post.id = Date.now()
-            this.$emit('create', this.post)
-            // this.post = {title: '', body: ''}
+        const onSubmit = (submitData) => {
+            const post = {
+                id,
+                title: submitData.title,
+                body: submitData.body,
+            }
+            emit('create', post)
         }
+
         return {
-            // title, body,
-            post: {
-                title,
-                body
-            },
+            title, body,
             onSubmit: handleSubmit(onSubmit),
         }
     },
-};
+}
 </script>
 
 <style scoped>
 .form_wrapper {
 
 }
-
 .addPost_title {
     text-align: center;
     margin-bottom: 20px;
 }
-
 .add_form {
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 5px;
 }
-
 .add_form input {
     width: 100%;
 }
-
 .form_input {
     width: 100%;
     height: 64px;
 }
-
 .input_error {
     color: red;
     text-align: center;
     margin-top: 4px;
 }
-
 .add_button {
     padding: 5px;
     cursor: pointer;
